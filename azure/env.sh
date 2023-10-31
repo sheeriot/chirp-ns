f5ab16ed0995453b424b7d49f9c042a18ecd9d0f
Kris Thompson <kris@austinvoicedata.com>
2023-10-31 22:32:11 +0000

    add network

--- azure/env.sh
+++ azure/env.sh
@@ -6,6 +6,7 @@
 # ------ Infra and Compute Name --------
 export INFRA_NAME=everychirp
 export TF_VAR_infra_name=$(echo "${INFRA_NAME}" | tr '[:upper:]' '[:lower:]')
+# net_name is set to infra_name for use within Terraform
 export TF_VAR_net_name=$TF_VAR_infra_name
 
 export COMPUTE_NAME=everychirp
@@ -22,23 +23,20 @@ export COMPONENT=$(echo "${COMPONENT}" | tr '[:upper:]' '[:lower:]')
 export TF_VAR_component=${COMPONENT}
 echo TF_VAR_component: ${TF_VAR_component}
 
-# clean the statefile suffix to eligible characters only
+# setup Storage account name. Clean non :alnum, convert to lower case, and limit length to 24 characters
 STATE_SUFFIX=$(echo $STATE_SUFFIX | tr -dc '[:alnum:]' | tr '[:upper:]' '[:lower:]')
-
 ENV_NAME_STOR=$(echo "${ENV_NAME}" | tr -dc '[:alnum:]' | tr '[:upper:]' '[:lower:]')
-
 # max string length for storage account name is 24 characters
 INFRA_NAME_STOR_MAX=$(expr 24 - ${#STATE_SUFFIX} - ${#ENV_NAME_STOR})
 echo INFRA_NAME_STOR_MAX: ${INFRA_NAME_MAX}
-
-# trucate $INFRA_NAME to 14 characters
+# clean infra_name for use as storage account prefix
 INFRA_NAME_STOR=$(echo "${INFRA_NAME}" | tr -dc '[:alnum:]' | tr '[:upper:]' '[:lower:]')
+# truncate infra_name to max length for storage acocunt
 INFRA_NAME_STOR=${INFRA_NAME_STOR:0:$INFRA_NAME_STOR_MAX}
-echo INFRA_NAME_STOR: ${INFRA_NAME_STOR}
-
 export TFSTATE_ACCOUNT=$(echo "${INFRA_NAME_STOR}${ENV_NAME_STOR}${STATE_SUFFIX}")
 echo TFSTATE_ACCOUNT: "${TFSTATE_ACCOUNT}"
 export TF_VAR_tfstate_account="${TFSTATE_ACCOUNT}"
+
 
 export TFSTATE_CONTAINER=tfstate
 echo TFSTATE_CONTAINER: ${TFSTATE_CONTAINER}
@@ -48,6 +46,7 @@ export TFSTATE_RG=$(echo "${INFRA_NAME}-${ENV_NAME}-OAM_RG" | tr '[:upper:]' '[:
 echo TFSTATE_RG: "${TFSTATE_RG}"
 export TF_VAR_tfstate_rg="${TFSTATE_RG}"
 
+# ARM Access key is used for access to TFState Storage Account
 echo ARM_ACCESS_KEY: ${ARM_ACCESS_KEY}
 
 CIDR_BLOCK=$(echo "${CIDR_BLOCK}" | tr -d '[:space:]')
